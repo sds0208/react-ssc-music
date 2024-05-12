f7f87ab46d690413e545fa59af51dd3e90649c0d
Sarah Santa Cruz <sarahsantacruz@Sarahs-Mac-mini.lan>
2024-05-12 10:56:32 -0500

    updating card content and layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import parse from 'html-react-parser'
 
 const Card = ({ sample }) => {
   const [audioPlaying, setAudioPlaying] = useState(false);
@@ -8,7 +9,10 @@ const Card = ({ sample }) => {
       {/* <FaMusic /> */}
       <h3 className="card-title">{sample.title}</h3>
       <p className="card-description">{sample.description}</p>
-      <audio controls src={`src/assets/${sample.audio}`} onPlay={() => setAudioPlaying(true)} onPause={() => setAudioPlaying(false)}></audio>
+      <audio controls className={`${sample.audio ? '' : 'hide'}`} src={`src/assets/${sample.audio}`} onPlay={() => setAudioPlaying(true)} onPause={() => setAudioPlaying(false)}></audio>
+      <div className={`${sample.percEns ? 'youtube-video': 'youtube-video hide'}`}>
+        { sample.video ? parse(sample.video) : <div className="video-placeholder">Video coming soon!</div> }
+      </div>
     </div>
   )
 }
--- src/data/music-samples.js
+++ src/data/music-samples.js
@@ -2,47 +2,47 @@ export const musicSamples = [
   {
     id: 1,
     title: 'Sample 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Commissioned by Lubbock Cooper HS\nFront Ensemble by Sarah Santa Cruz\nBattery by Rick Rodriguez',
     score: 'score.png',
-    audio: 'sc-22-p3.mp3',
+    audio: 'lc-24-p1-perc.mp3',
     featured: true
   },
   {
     id: 2,
     title: 'Sample 2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Commissioned by Shadow Creek HS\nFront Ensemble by Sarah Santa Cruz\nBattery by Rick Rodriguez',
     score: 'score.png',
-    audio: 'sc-22-p3.mp3'
+    audio: 'sc-24-spirited-away-perc.mp3',
+    featured: true
   },
   {
     id: 3,
     title: 'Sample 3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Commissioned by Shadow Creek HS\nFront Ensemble by Sarah Santa Cruz\nBattery by Rick Rodriguez',
     score: 'score.png',
-    audio: 'sc-22-p3.mp3'
+    audio: 'sc-22-p3-perc.mp3',
+    featured: true
   },
   {
     id: 4,
     title: 'Sample 4',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Commissioned by Cy Creek HS\nFront Ensemble by Sarah Santa Cruz\nBattery by Rick Rodriguez',
     score: 'score.png',
-    audio: 'sc-22-p3.mp3',
-    featured: true
+    audio: 'cc-22-perc.mp3'
   },
   {
     id: 5,
     title: 'Sample 5',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Commissioned by Lubbock Cooper HS\nFront Ensemble by Sarah Santa Cruz\nBattery by Rick Rodriguez',
     score: 'score.png',
-    audio: 'sc-22-p3.mp3'
+    audio: 'lc-23-p1-perc.mp3'
   },
   {
     id: 6,
     title: 'Sample 6',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Commissioned by Lubbock Cooper HS\nFront Ensemble by Sarah Santa Cruz\nBattery by Rick Rodriguez',
     score: 'score.png',
-    audio: 'sc-22-p3.mp3',
-    featured: true
+    audio: 'lc-23-p2-perc.mp3'
   },
 ];
 
@@ -50,38 +50,46 @@ export const percussionEnsembles = [
   {
     id: 7,
     title: 'A Cantiga',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    percEns: true,
+    description: 'For large percussion ensemble. Commissioned by the Spring High School Percussion Ensemble for their 2023 Midwest Clinic Performance.',
     score: 'score.png',
     audio: 'a-cantiga.mp3',
-    video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/PSmDvUNv064?si=Xu-n0qZ2o_F3XjvK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    video: '<iframe width="300" height="300" src="https://www.youtube.com/embed/PSmDvUNv064?si=Xu-n0qZ2o_F3XjvK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    linkText: 'Available for purchase at C.Alan',
     link: 'https://c-alanpublications.com/a-cantiga/',
     featured: true
   },
   {
     id: 8,
     title: 'Sky Music',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    percEns: true,
+    description: 'For low brass and percussion ensemble. Commissioned by the Cedar Ridge Low Brass Ensemble for their Midwest Clinic performance. Also performed at TMEA.',
     score: 'score.png',
+    linkText: 'Available for purchase at Tapspace',
     link: 'https://www.tapspace.com/percussion-ensemble/sky-music',
     audio: null,
-    video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/Sax2YQHyd8o?si=dE3pQLbtEu1ftRj-&amp;start=277" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
-  },
-  {
-    id: 9,
-    title: 'Brightly Shining',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    score: 'score.png',
-    link: 'https://www.jwpepper.com/10085602.item',
-    audio: null,
-    video: null
+    video: `<iframe width="300" height="300" src="https://www.youtube.com/embed/Sax2YQHyd8o?si=dE3pQLbtEu1ftRj-&amp;start=277" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
   },
   {
     id: 10,
     title: 'Into the Blue',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    percEns: true,
+    description: 'For percussion ensemble. Commissioned by the Roma High School Percussion Ensemble for their taping and performance at PASIC.',
     score: 'score.png',
+    linkText: 'Available for direct purchase',
     link: '/contact',
     audio: null,
-    video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/DY-3y5as4-Q?si=8OwfquuQpr-uJywL&amp;start=277" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+    video: `<iframe width="300" height="300" src="https://www.youtube.com/embed/DY-3y5as4-Q?si=8OwfquuQpr-uJywL&amp;start=277" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+  },
+  {
+    id: 9,
+    title: 'Brightly Shining',
+    percEns: true,
+    description: 'For percussion ensemble. Commissioned by the Spring High School Percussion Ensemble for their performance at Midwest Clinic in 2010.',
+    score: 'score.png',
+    linkText: 'Available for purchase at JW Pepper',
+    link: 'https://www.jwpepper.com/10085602.item',
+    audio: null,
+    video: null
   },
 ];
